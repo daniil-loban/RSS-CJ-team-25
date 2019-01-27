619512699f383a782db874408d003e0894d9e323
auseika <auseika.k@gmail.com>
2019-01-27 17:42:50 +0300

    feat: include <Gallery /> into <Architect />

--- src/components/architect/architect.js
+++ src/components/architect/architect.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Timeline from '../Timeline/Timeline';
 import Teammate from '../teammate/teammate';
+import Gallery from '../gallery/gallery';
 
 import './architect.css';
 
@@ -11,6 +12,7 @@ export default class Architect extends Component {
       <Timeline data={this.props.data} />
       <div className="architect__info">
         <Teammate data={this.props.data} isArchitect={true} />
+        <Gallery type="images" data={this.props.data} />
       </div>
     </section>;
   }
