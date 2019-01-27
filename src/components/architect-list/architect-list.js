7b244b0406fede04f4078dd585102c50c218f35a
daniil-loban <dungeon_lda@mail.ru>
2019-01-27 23:17:50 +0300

    feat: basic search

--- src/components/architect-list/architect-list.js
+++ src/components/architect-list/architect-list.js
@@ -7,15 +7,39 @@ import Teammate from '../teammate/teammate';
 import Search from '../search/search';
 
 class ArchitectList extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    const defaultData = props.data; // global.contentLang;
+    this.state = {
+      filtredData: defaultData,
+    };
+  }
+
   renderClickedArchitect(i) {
     ReactDOM.render(<Architect data={global.contentLang[i]} />, document.getElementById('content'));
   }
 
+  searchCallback(data) {
+    window.console.log('before Filtered Data', this.state.filtredData);
+    this.setState({ filtredData: data });
+    window.console.log('Filtered Data', this.state.filtredData);
+  }
+
   render() {
+    window.console.log('data in render', this.state.filtredData);
     return (
       <section className="architect-list">
-        <Search/>
-        {this.props.data.map((element, index) => <Teammate data={element} isArchitect={true} onClick={() => this.renderClickedArchitect(index)} />)}
+        <Search searchCallback={this.searchCallback} data = {global.contentLang}/>
+        {
+          Object.keys(this.state.filtredData).map(index => (
+            <Teammate
+              key = {index}
+              data = {this.state.filtredData[index]}
+              isArchitect = {true}
+              onClick={() => this.renderClickedArchitect(index)} />
+          ))
+        }
       </section>
     );
   }
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -3,16 +3,80 @@ import Icon from '@material-ui/core/Icon';
 import './search.css';
 
 export default class Search extends Component {
-  constructor({ props }) {
-    super();
+  constructor(props) {
+    super(props);
     this.props = props;
+    this.data = JSON.parse(JSON.stringify(this.props.data));
+    this.searchCallback = props.searchCallback;
+    this.state = {
+      inputValue: '',
+      isValueGetted: false,
+    };
+    this.updateInputValue = this.updateInputValue.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  setValueGetted() {
+    this.setState({
+      inputValue: this.state.inputValue,
+      isValueGetted: true,
+    });
+  }
+
+  search() {
+    const result = [];
+    if (this.state.inputValue.length === 0) return result;
+    const searchString = this.state.inputValue.trim().toLowerCase();
+    Object.keys(this.data).filter((item) => {
+      const allItemData = JSON.stringify(this.data[item]).toLowerCase();
+      if (allItemData.indexOf(searchString) !== -1) {
+        result.push(this.data[item]);
+      }
+      return allItemData.indexOf(searchString) !== -1;
+    });
+    return result;
+  }
+
+  handleKeyPress(event) {
+    if (event.key === 'Enter' && this.state.isValueGetted === false) {
+      this.setValueGetted();
+      if (this.searchCallback) this.searchCallback(this.search());
+    }
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    if (this.state.isValueGetted === false) {
+      this.setValueGetted();
+      if (this.searchCallback) this.searchCallback(this.search());
+    }
+  }
+
+  updateInputValue(event) {
+    this.setState({
+      inputValue: event.target.value,
+      isValueGetted: false,
+    });
   }
 
   render() {
     return (
       <form className = "search-form">
-        <input className = "search-form-input" placeholder = "search"/>
-        <button className = "search-form-button header-link"><Icon className="search-form-button-icon" >search</Icon></button>
+        <input
+          value = {this.state.inputValue}
+          onKeyPress = {this.handleKeyPress}
+          onChange = {this.updateInputValue}
+          className = "search-form-input"
+          placeholder = "search"
+        />
+        <button
+          onClick = {this.handleClick} 
+          className = "search-form-button header-link">
+            <Icon className="search-form-button-icon" >
+              search
+            </Icon>
+        </button>
       </form>
     );
   }
